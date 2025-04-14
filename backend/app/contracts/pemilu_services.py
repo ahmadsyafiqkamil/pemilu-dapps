a9bfa68865df97365d48b18b9cd60abcd9649d2c
ahmadsyafiqkamil <ahmadsyafiqkamil@gmail.com>
2025-04-14 11:11:16 +0700

    front page
    separate admin and voter page based on user role

--- backend/app/contracts/pemilu_services.py
+++ backend/app/contracts/pemilu_services.py
@@ -81,3 +81,7 @@ def build_transact(tx_function, user_address):
     tx["value"] = int(tx.get("value", 0))  # just in case
 
     return tx
+
+
+
+# add new admin: cast send 0x5FbDB2315678afecb367f032d93F642f64180aa3 "addAdmin(address)" 0xA85E11ed0Cb976B18Ba7F4D10180b635Fe24DEc0 --from 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,29 +1,51 @@
 'use client'
 
 import { useAccount } from 'wagmi'
-// import { useRouter } from 'next/navigation'
 import ConnectWalletNotice from '@/components/ui/ConnectWalletNotice'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { api } from '@/lib/api'
 
 export default function Home() {
-  const { isConnected } = useAccount()
-  // const router = useRouter()
+  const { isConnected, address } = useAccount()
+  const router = useRouter()
+  const [isChecking, setIsChecking] = useState(true)
 
-  // ⏩ Redirect jika wallet terkoneksi
-  // useEffect(() => {
-  //   if (isConnected) {
-  //     router.push('/notes')
-  //   }
-  // }, [isConnected, router])
+  // ⏩ Redirect berdasarkan role dari backend
+  useEffect(() => {
+    const checkAndRedirect = async () => {
+      if (isConnected && address) {
+        setIsChecking(true)
+        try {
+          const isAdmin = await api.checkAdminStatus(address)
+          if (isAdmin) {
+            router.push('/admin/dashboard')
+          } else {
+            router.push('/voter/dashboard')
+          }
+        } catch (error) {
+          console.error('Error during role check:', error)
+          // Jika terjadi error, arahkan ke halaman voter sebagai fallback
+          router.push('/voter/dashboard')
+        } finally {
+          setIsChecking(false)
+        }
+      }
+    }
+
+    checkAndRedirect()
+  }, [isConnected, address, router])
 
   // Jika belum connect, tampilkan notice
   if (!isConnected) {
     return <ConnectWalletNotice />
   }
 
-  // Render kosong/placeholder jika sedang proses redirect
+  // Render loading state saat mengecek role atau redirect
   return (
-    <div className="min-h-screen flex items-center justify-center text-muted">
-      Redirecting...
+    <div className="min-h-screen flex flex-col gap-2 items-center justify-center text-muted">
+      <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
+      <div>{isChecking ? "Memeriksa status admin..." : "Mengalihkan..."}</div>
     </div>
   )
 }
